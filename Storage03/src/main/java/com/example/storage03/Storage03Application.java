84e5eb59331f793a6b3e39c657bfff768ae770de
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-10-26 15:53:04 -0300

    arquivos esquecidos

---
+++ Storage03/src/main/java/com/example/storage03/Storage03Application.java
@@ -0,0 +1,13 @@
+package com.example.storage03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Storage03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Storage03Application.class, args);
+    }
+
+}
---
+++ Storage03/src/test/java/com/example/storage03/Storage03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storage03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Storage03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
